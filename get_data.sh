ed4863aa47702220ff82e542306a5b5a78e7cb55
Rob Aitchison <raitch@sawed-off.com>
2018-08-19 11:51:07 -0400

    Create get_data.sh

---
+++ get_data.sh
@@ -0,0 +1,22 @@
+# Tracking the steps I perform using my Mac - the goal is to re-factor after doing things manually and make this cross-platform
+
+curl -O http://areadownloads.autodesk.com/oc/hsm/svenwetdrydirty.zip
+curl -O http://areadownloads.autodesk.com/oc/hsm/trilobot%20v2.zip
+curl -O http://areadownloads.autodesk.com/oc/hsm/satellite.zip
+curl -O http://areadownloads.autodesk.com/oc/hsm/ship.zip
+curl -O http://areadownloads.autodesk.com/oc/hsm/communicator%20v2.zip
+curl -O http://areadownloads.autodesk.com/oc/hsm/wrench.zip
+curl -O http://areadownloads.autodesk.com/oc/hsm/killamari.zip
+curl -O http://areadownloads.autodesk.com/oc/hsm/beach.zip
+curl -O http://areadownloads.autodesk.com/oc/hsm/juggernaut.zip
+curl -O http://areadownloads.autodesk.com/oc/hsm/planet.zip
+
+# unzip "*.zip" - commented out, does not work, weird...
+
+# Directory mismatch - some are in hsm folder, some are not
+# remove hsm/asset/character/juggernaut folder
+# then move the juggernaut folder there
+# move sven to asset/character dir
+# move prop to asset prop dir
+# make asset/environment folder
+# move beach and planet to the above folder
